2db40b51b69962173ba81560004c3e71929f9074
Ayudh <ghari81189+git@gmail.com>
2016-05-14 11:33:52 +0530

    What

--- Welcome.java
+++ Welcome.java
@@ -2,7 +2,7 @@ class Welcome
 {
     public static void main(String[] atgs)
     {
-        //this line prints hello world so what
+        //this line prints hello world so 
         System.out.println("Hello World");
     }
 }
